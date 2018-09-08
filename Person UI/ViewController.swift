b66db5a501ad4cd4971fed2b2713330c2bc1c0a7
123 <a123@123demacbook.local>
2018-09-07 22:41:55 -0500

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by 123 on 2018/9/7.
+//  Copyright © 2018年 Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
